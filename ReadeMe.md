a8ede94cc24ac6eb87d171285d7859d54f46e6ff
huangzhipeng <huangzp50@chinaunicom.cn>
2023-03-15 16:37:50 +0800

    init: commit all

---
+++ ReadeMe.md
@@ -0,0 +1,107 @@
+# CUCObsSample
+cuc obs是[联通云](https://www.cucloud.cn/product/oss.html)对外提供的一款对象存储产品，它在设计上严格遵守AWS S3协议，所以很大程度的兼容了AWS S3的各类sdk。
+这里给出了使用[aws s3的.Net sdk](https://docs.aws.amazon.com/sdkfornet/v3/apidocs/items/S3/NS3.html)来连接联通云的对象存储服务的示例。
+此处使用的sdk版本为
+| sdk         |  version   |
+| :---------- | :--------: |
+| .Net        |  7.0.201   |
+| AWSSDK.Core | 3.7.105.18 |
+| AWSSDK.S3   | 3.7.103.26 |
+
+
+# dotnet 示例项目
+## 1. 创建项目
+```
+mkdir dotnet
+cd dotnet 
+
+dotnet new sln
+dotnet new console --output ./cucsample
+dotnet sln add ./cucsample 
+```
+## 2. 添加sdk
+```
+cd cucsample
+dotnet add package Amazon.S3
+dotnet add package AWSSDK.Core
+```
+## 3. 快速使用
+链接对象存储服务
+```C#
+AmazonS3Config conf = new AmazonS3Config
+{
+    ServiceURL = Endpoint,
+};
+AmazonS3Client client = new AmazonS3Client(AccessKey, SecretKey, conf);
+```
+创建存储桶
+```C# 
+var request = new PutBucketRequest
+{
+    BucketName = Bucket,
+    UseClientRegion = true,
+};
+
+var response = await client.PutBucketAsync(request);
+```
+列举用户空间下的存储桶
+```C#
+var request = new ListBucketsRequest();
+ListBucketsResponse response = await client.ListBucketsAsync();
+```
+上传对象到存储桶中
+```C#
+ var request = new PutObjectRequest
+{
+    BucketName = bucketName,
+    Key = keyName,
+    FilePath = filePath,
+};
+var response = await client.PutObjectAsync(request);
+```
+列举存储桶中的对象
+```C#
+var request = new ListObjectsV2Request
+{
+    BucketName = bucket,
+    MaxKeys = 5,
+};
+ListObjectsV2Response response;
+do
+{
+    response = await client.ListObjectsV2Async(request);
+    response.S3Objects.ForEach(obj => Console.WriteLine($"\t object: {obj.Key}, Size:{obj.Size}"));
+}
+while (response.IsTruncated);
+```
+删除存储桶中的对象
+```C#
+var response = await client.DeleteObjectAsync(bucketName,key);
+```
+删除存储桶
+```C#
+var request = new DeleteBucketRequest
+{
+    BucketName = bucketName,
+};
+var response = await client.DeleteBucketAsync(request);
+```
+## 4. 示例代码运行结果
+```shell
+[root]_ ~/CucObsSample/dotnet>: dotnet run
+##### cuc sdk for .net samples #####
+-->: 1.create bucket 
+         Successfully create bucket:newBucket.
+-->: 2.list bucket 
+         bucket: newbucket
+-->: 3.upload object 
+         Successfully upload Program.cs to newbucket
+-->: 4.list object from bucket 
+         object: Program.cs, Size:6510
+-->: 5.delete object from bucket 
+         successfully delete object: {key}
+-->: 6. delete bucket 
+         Successfully delete bucket: newbucket
+```
+## 5. 其它
+示例项目中只是给出了sdk中几个较为常用的方法，其它方法的是使用可以参考aws s3的官方文档
---
+++ dotnet/Program.cs
@@ -0,0 +1,183 @@
+﻿using System.Reflection;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace cuc.obs.sample
+{
+    public class Program
+    {
+        // please change this special value to your real access key
+        public static string AccessKey = "ak";
+        // please change this special value to your real secret key 
+        public static string SecretKey = "sk";
+        // cuc obs endpoint .example: http://obs-lf.cucloud.cn
+        public static string Endpoint = "http://endpoint";
+        // bucket name
+        public static string Bucket = "newBucket";
+
+        //create a bucket
+        public static async Task CreateBucket(AmazonS3Client client)
+        {
+            try
+            {
+                var request = new PutBucketRequest
+                {
+                    BucketName = Bucket,
+                    UseClientRegion = true,
+                };
+
+                var response = await client.PutBucketAsync(request);
+                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    Console.WriteLine($"\t Successfully create bucket:{Bucket}.");
+                }
+                else
+                {
+                    Console.WriteLine($"Could not create bucket: {Bucket}.");
+                }
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine($"Error creating bucket: '${e.Message}'");
+            }
+        }
+
+        //list buckets
+        public static async Task ListBucket(AmazonS3Client client)
+        {
+            try
+            {
+                var request = new ListBucketsRequest();
+                ListBucketsResponse response = await client.ListBucketsAsync();
+                response.Buckets.ForEach(bucket => Console.WriteLine($"\t bucket: {bucket.BucketName}"));
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine($"Error list buckets. Message:'{e.Message}' getting list of objects.");
+            }
+        }
+
+        //upload object
+        public static async Task UploadObject(AmazonS3Client client, string bucketName)
+        {
+            string filePath = Directory.GetCurrentDirectory() + "/Program.cs";
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"Could not find {filePath}");
+            }
+            string keyName = Path.GetFileName(filePath);
+
+            try
+            {
+                var request = new PutObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName,
+                    FilePath = filePath,
+                };
+
+                var response = await client.PutObjectAsync(request);
+                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    Console.WriteLine($"\t Successfully upload {request.Key} to {bucketName}");
+                }
+                else
+                {
+                    Console.WriteLine($"\t Could not upload objet to bucket: {bucketName}");
+                }
+            }
+            catch (System.Net.WebException e)
+            {
+                Console.WriteLine($"Error create objet. Message:'{e.Message}'. ");
+            }
+        }
+
+        // list object from bucket
+        public static async Task ListObjects(AmazonS3Client client, string bucket)
+        {
+            try
+            {
+                var request = new ListObjectsV2Request
+                {
+                    BucketName = bucket,
+                    MaxKeys = 5,
+                };
+                ListObjectsV2Response response;
+                do
+                {
+                    response = await client.ListObjectsV2Async(request);
+                    response.S3Objects.ForEach(obj => Console.WriteLine($"\t object: {obj.Key}, Size:{obj.Size}"));
+                }
+                while (response.IsTruncated);
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine($"Error list object. Message:'{e.Message}'.");
+            }
+        }
+
+        // delete objet from bucket
+        public static async Task DeleteObject(AmazonS3Client client, string bucketName, string key)
+        {
+            try
+            {
+                var response = await client.DeleteObjectAsync(bucketName, key);
+                Console.WriteLine("\t successfully delete object: {key}");
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine($"\t Could not delete object: {0}", key);
+                Console.WriteLine($"Error delete object. Message:'{e.Message}");
+            }
+        }
+
+        // delete Bucket
+        public static async Task DeleteBucket(AmazonS3Client client, string bucketName)
+        {
+            var request = new DeleteBucketRequest
+            {
+                BucketName = bucketName,
+            };
+            var response = await client.DeleteBucketAsync(request);
+            if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine($"\t Successfully delete bucket: {bucketName}");
+            }
+            else
+            {
+                Console.WriteLine($"\t Successfully delete bucket: {bucketName}");
+            }
+        }
+
+        public static async Task Main()
+        {
+            Console.WriteLine("##### cuc sdk for .net samples #####");
+
+            AmazonS3Config conf = new AmazonS3Config
+            {
+                ServiceURL = Endpoint,
+            };
+            AmazonS3Client client = new AmazonS3Client(AccessKey, SecretKey, conf);
+
+            Console.WriteLine($"-->: 1.create bucket ");
+            await CreateBucket(client);
+
+            Console.WriteLine($"-->: 2.list bucket ");
+            await ListBucket(client);
+
+            Console.WriteLine("-->: 3.upload object ");
+            await UploadObject(client, "newbucket");
+
+            Console.WriteLine("-->: 4.list object from bucket ");
+            await ListObjects(client, "newbucket");
+
+            Console.WriteLine("-->: 5.delete object from bucket ");
+            await DeleteObject(client, "newbucket", "Program.cs");
+
+            Console.WriteLine("-->: 6. delete bucket ");
+            await DeleteBucket(client, "newbucket");
+        }
+    }
+
+}
